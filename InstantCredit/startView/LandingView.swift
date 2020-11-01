7ffbbdc5744cf2f00901198d21e58cf2189f0ef2
CS193P student <yiu.jackie@gmail.com>
2020-11-01 01:16:36 -0500

    Completed Carousel View

--- InstantCredit/startView/LandingView.swift
+++ InstantCredit/startView/LandingView.swift
@@ -12,6 +12,10 @@ struct LandingView: View {
 
     @State var selection: Int? = nil
 
+    @State var carouselPane: Int = 0
+
+    
+    
     var body: some View {
                 VStack(alignment: .center) {
                     
@@ -20,37 +24,51 @@ struct LandingView: View {
                         Image("yoga").resizable()
                                 .frame(width: 100, height: 50)
                         
-                        Text("LifeLink")
+                        Text("AllTrails")
                             .foregroundColor(Color(.gray))
-                            .font(.largeTitle)
+                            .font(.title)
                     }
                         .padding()
 //                    Spacer()
-                    GeometryReader { geometry in
-                        CarouselView()
+//                    withAnimation {
+                        GeometryReader { geometry in
+                        CarouselView(geoemtry: geometry, carouselPane: carouselPane)
                     
+//                            .frame(width: geometry.size.width, height: geometry.size.height)
+                            .offset(CGSize(width: panOffset.width, height: 0))
+                    }
+                        .edgesIgnoringSafeArea(.horizontal)
+                        .gesture(self.panGesture())
+                        .animation(.easeInOut)
+
+//                    }
+                    GeometryReader { geometry in
                     
                         HStack {
                             Spacer(minLength: geometry.size.width/2.5)
                             HStack {
-                                Circle().frame(width: 10, height: 10)
-                                Circle().frame(width: 10, height: 10)
-                                Circle().frame(width: 10, height: 10)
-                                Circle().frame(width: 10, height: 10)
+                                Circle().foregroundColor(Color(carouselPane == 0 ? .black : .lightGray))
+                                Circle().foregroundColor(Color(carouselPane == 1 ? .black : .lightGray))
+                                Circle().foregroundColor(Color(carouselPane == 2 ? .black : .lightGray))
+                                Circle().foregroundColor(Color(carouselPane == 3 ? .black : .lightGray))
                             }
                             Spacer(minLength: geometry.size.width/2.5)
                         }
-                            .frame(maxHeight: 20)
+                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.04)
+                            
+                    }
+                        .frame(maxHeight: 20)
+                    VStack(alignment: .leading) {
+                        Text(text1[carouselPane])
+                            .padding(6)
+                            .font(.headline)
+                        Text(text2[carouselPane])
+                            .padding(6)
+                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.08)
+                            .font(.subheadline)
+
                     }
                         
-                    VStack(alignment: .leading) {
-                        Text("Discover the wonders of the world today!").font(.headline).padding()
-                        Text("""
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce condimentum risus dolor, id vehicula nibh.
-                            """)
-                            .font(.subheadline)
-                            .padding()
-                    }
 //                    Spacer()
                     HStack {
                         NavigationLink(destination: SignUpView(), tag: 1, selection: $selection) {
@@ -62,11 +80,6 @@ struct LandingView: View {
                                 .foregroundColor(Color(UIColor.mainColor))
                                 .background(Color(.white))
                                 .padding()
-                        
-
-
-
-                                
                         }
                         NavigationLink(destination: LoginView(), tag: 2, selection: $selection) {
                             Button(action: {
@@ -83,64 +96,117 @@ struct LandingView: View {
             }
                 .navigationBarHidden(true)
                 .navigationBarTitle(Text("Home"))
-                .edgesIgnoringSafeArea(.horizontal)
 //                .background(Color(UIColor.mainColor))
         }
+    @State var steadyStatePanOffset: CGSize = .zero
+    @GestureState var gesturePanOffset: CGSize = .zero
+
+    private var panOffset: CGSize {
+        (steadyStatePanOffset + gesturePanOffset)
     }
+
+
+    private func panGesture() -> some Gesture {
+        DragGesture()
+            .updating($gesturePanOffset) { latestDragGestureValue, gesturePanOffset, transaction in
+                
+                if carouselPane != 0 && latestDragGestureValue.translation.width > 0 || carouselPane != 3 && latestDragGestureValue.translation.width < 0 {
+                    gesturePanOffset = latestDragGestureValue.translation
+                }
+        }
+        .onEnded { finalDragGestureValue in
+            print(finalDragGestureValue.translation)
+            
+            if carouselPane != 0 && finalDragGestureValue.translation.width > 0 && finalDragGestureValue.translation.width > (UIScreen.main.bounds.width * 0.3) {
+                steadyStatePanOffset = steadyStatePanOffset + CGSize(width: UIScreen.main.bounds.width, height: 0)
+                carouselPane -= 1
+            }
+            
+            if carouselPane != 3 && finalDragGestureValue.translation.width < 0 && (finalDragGestureValue.translation.width * -1) > (UIScreen.main.bounds.width * 0.3) {
+                steadyStatePanOffset = steadyStatePanOffset - CGSize(width: UIScreen.main.bounds.width, height: 0)
+                carouselPane += 1
+            }
+
+//            steadyStatePanOffset = steadyStatePanOffset + (finalDragGestureValue.translation)
+            print(steadyStatePanOffset)
+            print(UIScreen.main.bounds.width)
+        }
+    }
+}
+
+
+
 
 
 struct CarouselView: View {
     
+    var passedCarouselPane: Int
+    var passedGeometry: GeometryProxy
+    
+    init(geoemtry: GeometryProxy, carouselPane: Int){
+        self.passedGeometry = geoemtry
+        self.passedCarouselPane = carouselPane
+    }
+    
     var body: some View {
         
 
-        CardView()
+        CardView(geometry: passedGeometry, carouselPane: passedCarouselPane)
     }
 }
 
 struct CardView: View {
+    var passedCarouselPane: Int
+    var passedGeometry: GeometryProxy
+    
+    init(geometry: GeometryProxy, carouselPane: Int) {
+        self.passedGeometry = geometry
+        self.passedCarouselPane = carouselPane
+    }
     var body: some View {
-        HStack {
-            GeometryReader {geometry in
+        HStack(spacing: 0) {
                 ForEach(cardList, id: \.id) { cards in
-                    Image(cards.fileName)
-                        .resizable()
-                        .frame(width: geometry.size.width, height: geometry.size.width)
-                        .padding()
+                    Image(cards.fileName).centerCropped()
+                        
+//                        .resizable()
+                        .frame(width: passedGeometry.size.width, height: passedGeometry.size.height)
+                        .opacity(cards.id == passedCarouselPane+1 ? 1 : 0.6)
+                        
                 }
-            }
-
         }
-        .padding()
-            
     }
 }
 
 var cardList: Array<Card> {
     [
-        Card(fileName: "p1", label: "pikachu"),
-        Card(fileName: "p2", label: "bulbasaur"),
-        Card(fileName: "p3", label: "charmander"),
-        Card(fileName: "p4", label: "squirtle")
+        Card(id: 1, fileName: "p1-1", label: "pikachu"),
+        Card(id: 2, fileName: "p2-2", label: "bulbasaur"),
+        Card(id: 3, fileName: "p3-3", label: "charmander"),
+        Card(id: 4, fileName: "p4-4", label: "squirtle")
     ]
 }
 
 struct Card {
     
-    var id = UUID()
+    var id: Int
     var fileName: String
     var label: String
 }
 
+var text1 = ["Discover 100,000+ trails around the world", "Find a trail that's perfect for you", "Hit the trail with confidence", "Get driving directions right to the trailhead"]
 
-
-
-
-
-
-
-
-
+var text2 = ["""
+            Explore the largest collection of trail maps anywhere, curated by millions of outdoor ethusiasts like you.
+            """,
+            """
+            Find your perfect hike, bike ride, or run. FIlter by length, rating, and difficulty. Easily find dog and kid-friendly trails.
+            """,
+            """
+            Browse hand-curated trail maps plus reviews, photos and recordings from the AllTrails community.
+            """,
+            """
+            With just one tap, get detailed driving directions so you can quickly and easily get right to the trailhead
+            """]
 
 
 
--- InstantCredit/startView/LandingViewHelpers.swift
+++ InstantCredit/startView/LandingViewHelpers.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 
 func validateFields(email: String, password: String) -> String {
@@ -32,4 +33,49 @@ func isPasswordValid(_ password: String) -> Bool {
 
 func findCredentials() {
     return 
+}
+
+extension CGPoint {
+    static func -(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.x - rhs.x, height: lhs.y - rhs.y)
+    }
+    static func +(lhs: Self, rhs: CGSize) -> CGPoint {
+        CGPoint(x: lhs.x + rhs.width, y: lhs.y + rhs.height)
+    }
+    static func -(lhs: Self, rhs: CGSize) -> CGPoint {
+        CGPoint(x: lhs.x - rhs.width, y: lhs.y - rhs.height)
+    }
+    static func *(lhs: Self, rhs: CGFloat) -> CGPoint {
+        CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
+    }
+    static func /(lhs: Self, rhs: CGFloat) -> CGPoint {
+        CGPoint(x: lhs.x / rhs, y: lhs.y / rhs)
+    }
+}
+
+extension CGSize {
+    static func +(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+    }
+    static func -(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
+    }
+    static func *(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
+    }
+    static func /(lhs: Self, rhs: CGFloat) -> CGSize {
+        CGSize(width: lhs.width/rhs, height: lhs.height/rhs)
+    }
+}
+
+extension Image {
+    func centerCropped() -> some View {
+        GeometryReader { geo in
+            self
+            .resizable()
+            .scaledToFill()
+            .frame(width: geo.size.width, height: geo.size.height)
+            .clipped()
+        }
+    }
 }
